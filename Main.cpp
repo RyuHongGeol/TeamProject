4191a4f30069f5664150d278f25c36f2443f260a
Honggeol <sfsysryu@gmail.com>
2021-11-01 10:00:13 +0900

    first commit

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <thread>
+
+void PrintMessage(const std::string& message, std::chrono::seconds delay)
+{
+	std::cout << "Sleep now...";
+
+	auto end = std::chrono::high_resolution_clock::now() + delay;
+
+	while (std::chrono::high_resolution_clock::now() < end)
+	{
+		std::this_thread::yield();
+	}
+
+	std::cout << message << std::endl;
+
+}
+
+int main()
+{
+	std::thread thread(PrintMessage, "Message From a child thread", std::chrono::seconds(3));
+	
+	PrintMessage("Message from a main thread",std::chrono::seconds(2));
+
+	thread.join();
+
+	return 0;
+}
